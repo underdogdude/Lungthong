7dca1175bac675a301f596aa8f80742aeb474a02
big14531 <big14531@hotmail.com>
2014-12-16 22:52:58 +0700

    split register page

--- HTML/dist/js/login.js
+++ HTML/dist/js/login.js
@@ -1,27 +1,7 @@
-window.onload=fade;
+/*window.onload=fade;
 function fade(){
 	sleep(2000);
 	document.getElementById('spinner').style.display='none';
 	document.getElementById('button').style.display='block';
 }
-function showlogin(){
-	document.getElementById('button').style.display='none';
-	document.getElementById('loginform').style.display='block';
-	
-}
-function showregister(){
-	document.getElementById('logo').style.display='none';
-	document.getElementById('loginform').style.display='none';
-	document.getElementById('button').style.display='none';
-	document.getElementById('registerform').style.display='block';
-	
-}
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
+*/
